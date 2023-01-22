9aff444fa4fe2e387650213f9a071aea43c9d303
Alabi Temiloluwa <temiloluwaalabi33@gmail.com>
2023-01-22 19:50:14 +0100

    credentials updated

--- server.js
+++ server.js
@@ -34,6 +34,9 @@ import handleNewAdmin from "./controllers/RegisterAdmin.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 dotenv.config()
+// app.use(cors())
+app.use(credentials);
+app.use(cors(corsOptions));
 const app = express();
 app.use(express.json());
 app.use(helmet())
@@ -43,9 +46,7 @@ app.use(morgan("common"));
 
 app.use(bodyParser.json({limit: "30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
-// app.use(cors())
-app.use(credentials);
-app.use(cors(corsOptions));
+
 app.use(cookieParser());
 
 const PORT = process.env.PORT || 5800;
